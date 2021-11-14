2bf7b9004471f7e215cc59329a81c49caf5815cb
Tengku Arif <arereeftengku@gmail.com>
2021-11-14 21:26:20 +0800

    initial commit

---
+++ utils.js
@@ -0,0 +1,38 @@
+const utils = {
+  widthGrid(n) {
+    return n * 16;
+  },
+  asGridCoord(x, y) {
+    return `${x * 16},${y * 16}`;
+  },
+  nextPosition(initialX, initialY, direction) {
+    let x = initialX;
+    let y = initialY;
+    const size = 16;
+    if (direction === "left") {
+      x -= size;
+    } else if (direction === "right") {
+      x += size;
+    } else if (direction === "up") {
+      y -= size;
+    } else if (direction === "down") {
+      y += size;
+    }
+    return { x, y };
+  },
+  oppositeDirection(direction) {
+    if (direction === "left") return "right";
+    if (direction === "right") return "left";
+    if (direction === "up") return "down";
+    return "up";
+  },
+  wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  },
+  emitEvent(name, detail) {
+    const event = new CustomEvent(name, {
+      detail,
+    });
+    document.dispatchEvent(event);
+  },
+};
