c4d9ab065444167f2b09d23afc81a377fe1445f9
Tengku Arif <arereeftengku@gmail.com>
2021-12-01 03:44:16 +0800

    Pizza Team HUD

--- Battle/BattleEvent.js
+++ Battle/BattleEvent.js
@@ -54,6 +54,9 @@ class BattleEvent {
 
     await utils.wait(600);
 
+    this.battle.playerTeam.update();
+    this.battle.enemyTeam.update();
+
     target.pizzaElement.classList.remove("battle-damage-blink");
     resolve();
   }
@@ -101,6 +104,9 @@ class BattleEvent {
     this.battle.activeCombatants[replacement.team] = replacement.id;
     replacement.update();
     await utils.wait(400);
+
+    this.battle.playerTeam.update();
+    this.battle.enemyTeam.update();
 
     resolve();
   }
